cef1919f6e0dd6b2a0ead3b529de35ec1fcaa989
User Server Web 192.168.26.26 <portalweb@ipd.gob.pe>
2018-01-14 11:07:05 -0500

    conexion sqlserver y pdf

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new AkademiaBundle\AkademiaBundle(),
-            new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
+            new Realestate\MssqlBundle\RealestateMssqlBundle(),
+            //new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
