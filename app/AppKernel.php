2eab2ad4b8b493c943936b3b996cdf0d72b3be60
majerhua <majerhua123@gmail.com>
2018-01-14 15:34:36 -0500

    subiendo cambios ultimos de verdad pdf

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,20 +17,15 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new AkademiaBundle\AkademiaBundle(),
-<<<<<<< HEAD
-            new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
             new WhiteOctober\TCPDFBundle\WhiteOctoberTCPDFBundle(),
-=======
             new Realestate\MssqlBundle\RealestateMssqlBundle(),
-            //new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
->>>>>>> 75d4d4cb92ccba26e4aa377a6664609ef0ded467
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-
+            
             if ('dev' === $this->getEnvironment()) {
                 $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
                 $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
